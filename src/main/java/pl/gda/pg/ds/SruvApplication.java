9b0032d61252594edc2476e242622a702a4c5dd2
alan <alan@ds.pg.gda.pl>
2016-11-13 13:23:52 +0100

    initial version bootstrapped from spring initializr

---
+++ src/main/java/pl/gda/pg/ds/SruvApplication.java
@@ -0,0 +1,12 @@
+package pl.gda.pg.ds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SruvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SruvApplication.class, args);
+	}
+}
