9996f8a68a6e251d4d8943518bcb35d9e45d7863
alan <alan@ds.pg.gda.pl>
2016-11-13 18:29:46 +0100

    Swagger and a integration test

--- src/main/java/pl/gda/pg/ds/sruv/controllers/UserController.java
+++ src/main/java/pl/gda/pg/ds/sruv/controllers/UserController.java
@@ -1,10 +1,8 @@
 package pl.gda.pg.ds.sruv.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import pl.gda.pg.ds.sruv.exceptions.UserAlreadyExistsException;
 import pl.gda.pg.ds.sruv.models.User;
 import pl.gda.pg.ds.sruv.services.UserService;
@@ -24,6 +22,7 @@ public class UserController {
     }
 
     @RequestMapping(method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
     public User createUser(@RequestBody  final User user) throws UserAlreadyExistsException {
         return service.save(user);
     }
