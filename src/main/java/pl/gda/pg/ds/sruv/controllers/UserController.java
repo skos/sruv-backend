986cb7e7ac1d6429f7a4e027d8bfe03ca724a30a
alan <alan@ds.pg.gda.pl>
2016-11-27 11:18:21 +0100

    Postgres backend

--- src/main/java/pl/gda/pg/ds/sruv/controllers/UserController.java
+++ src/main/java/pl/gda/pg/ds/sruv/controllers/UserController.java
@@ -3,8 +3,8 @@ package pl.gda.pg.ds.sruv.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import pl.gda.pg.ds.sruv.dtos.SimpleUser;
 import pl.gda.pg.ds.sruv.exceptions.UserAlreadyExistsException;
-import pl.gda.pg.ds.sruv.models.User;
 import pl.gda.pg.ds.sruv.services.UserService;
 
 import javax.validation.Valid;
@@ -18,13 +18,13 @@ public class UserController {
     private UserService service;
 
     @RequestMapping(method = RequestMethod.GET)
-    public List<User> findAll(){
+    public List<SimpleUser> findAll(){
         return service.findAll();
     }
 
     @RequestMapping(method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.CREATED)
-    public User createUser(@RequestBody @Valid final User user) throws UserAlreadyExistsException {
-        return service.save(user);
+    public void createUser(@RequestBody @Valid final SimpleUser user) throws UserAlreadyExistsException {
+        service.save(user);
     }
 }
--- src/main/java/pl/gda/pg/ds/sruv/services/UserService.java
+++ src/main/java/pl/gda/pg/ds/sruv/services/UserService.java
@@ -1,12 +1,12 @@
 package pl.gda.pg.ds.sruv.services;
 
+import pl.gda.pg.ds.sruv.dtos.SimpleUser;
 import pl.gda.pg.ds.sruv.exceptions.UserAlreadyExistsException;
-import pl.gda.pg.ds.sruv.models.User;
 
 import java.util.List;
 
 public interface UserService {
-    User save(User user) throws UserAlreadyExistsException;
+    void save(SimpleUser user) throws UserAlreadyExistsException;
 
-    List<User> findAll();
+    List<SimpleUser> findAll();
 }
